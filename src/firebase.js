e4bb62b39efbf7e8ed0071b12f8346c3c72e8199
awxinmin <awxinmin@gmail.com>
2020-03-29 18:29:09 +0800

    week 10

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,17 @@
 import firebase from 'firebase';
 
-const firebaseConfig = {
-    //Key in your database details
-    apiKey: "",
-    authDomain: "",
-    databaseURL: "",
-    projectId: ""
 
-  };
-  
+const firebaseConfig = {
+    apiKey: "AIzaSyC6jQfDe0IaMH1b4S09RL5CUpvfgNHUTBE",
+    authDomain: "week9-eff5a.firebaseapp.com",
+    databaseURL: "https://week9-eff5a.firebaseio.com",
+    projectId: "week9-eff5a",
+    storageBucket: "week9-eff5a.appspot.com",
+    messagingSenderId: "109127557261",
+    appId: "1:109127557261:web:1b80df6596a1f7168b82c4",
+    measurementId: "G-92KG3T0GKY"
+
+};
   firebase.initializeApp(firebaseConfig);
   var database = firebase.firestore();
   export default database;
